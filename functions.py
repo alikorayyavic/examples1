d31aa2c9c441b3788f060d8212a41c5fae6168dc
alikorayyavic <alikorayyavic@gmail.com>
2017-05-25 11:41:09 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,86 @@
+def f(x):
+	return x+10	
+	
+print(f(8))
+
+def hi(parametre):
+	if(type(parametre)==int):
+		print("hi"+str(parametre))
+	else:	
+		print("hi"+parametre)
+
+hi("koray")
+hi("ali")
+hi(10)
+
+
+def fib(n):
+    a,b = 0,1
+    while a<n:
+		print(a)
+		a,b=b,a+b
+
+
+fib(20)
+
+
+def fact(n):
+	a = 1
+	result = 1
+	while (a <= n):
+		result = result * a
+		a = a + 1
+	return result
+		
+print(fact(5))	
+
+#n!=n*(n-1)	
+
+def facto(n):
+	if(n==0):
+		return 1
+	return n*facto(n-1)	
+
+print(facto(5)) 
+
+
+
+#g(x,y)
+
+def g(x,y=3):
+	return  x + 5*y
+
+print(g(2,5))
+print(g(2))	  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
